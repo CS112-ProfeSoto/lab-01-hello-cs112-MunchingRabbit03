86dfd76607a10430915a38306bf3dd1029dabb7d
MunchingRabbit03 <eduardomartin163@yahoo.com>
2025-06-12 07:04:48 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        Book book1 = new Book ("Diary of the Wimpy Kid");
+        Book book2 = new Book("Harry Potter");
+        Book book3 = new Book("Catch me If you can");
+        Book anotherBook = new Book("Catch me If you can");
+
         System.out.println("Welcome to CS 112!");
         nightOwl();
+
         int result = subtractOne(1);
         System.out.println("Subtracting! 1 from 1:  " + result);
+        
+        System.out.println(book1);
+        System.out.println(book3);
+
+        System.out.println(book1.equals(book2));
+        System.out.println(book3.equals(anotherBook));
     }
 
     public static void nightOwl() {
@@ -13,7 +25,7 @@ public class Main {
     public static int subtractOne(int number) {
         return number - number;
     }
-    
+
 
 
 }
